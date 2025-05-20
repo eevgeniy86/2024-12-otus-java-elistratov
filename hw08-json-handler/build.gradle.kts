36b36e9ff84f42644c8cdc98c477a713438b960f
eevgeniy86 <eelistratov1986@yandex.ru>
2025-05-21 01:29:41 +0300

    modified:   hw08-json-handler/build.gradle.kts

--- hw08-json-handler/build.gradle.kts
+++ hw08-json-handler/build.gradle.kts
@@ -1,8 +1,6 @@
 dependencies {
     implementation ("ch.qos.logback:logback-classic")
     implementation("com.fasterxml.jackson.core:jackson-databind")
-
-
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
     testImplementation ("org.assertj:assertj-core")
